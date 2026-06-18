72dc751903771e497a60275f8ef833690a85da6a
Iris Valentina Gil Barrios <iris.gil@grupolistos.net>
2026-06-18 15:30:21 -0500

    Operativo Dashboard

--- views/nhc.py
+++ views/nhc.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 def show():
     st.title("NHC")
+        
--- views/operativo.py
+++ views/operativo.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 def show():
-    st.title("OPERATIVO")
+    st.components.v1.iframe(
+     "https://app.powerbi.com/view?r=eyJrIjoiMGZlNTJiNjQtMDlhMi00MTRlLWE2YjktOTUzMTUwZmJkMzI4IiwidCI6ImQyYmMyMDhlLTRmZmYtNDA3OC1iNWFjLWY2OWE2YTI2NmZmMyIsImMiOjR9",
+    height=800
+        )
